a51c95c851c0e604bf6eb2ebf8ff395884907b77
Pazy0g <renot.ibep56@icloud.com>
2023-03-22 17:16:52 +0100

    commit

---
+++ static/js/functions.js
@@ -0,0 +1,19 @@
+let pseudo = document.getElementById('inputUid');
+let password = document.getElementById('inputPwd');
+let button = document.getElementById("loginSubmit");
+
+// Fonction pour mettre à jour l'état du bouton
+function updateButtonState() {
+    if (pseudo.value && password.value) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
+
+// Écouteurs d'évènement sur les champs
+pseudo.addEventListener('input', updateButtonState);
+password.addEventListener('input', updateButtonState);
+
+// Initialise l'état du bouton
+updateButtonState();
--- view/connexion.php
+++ view/connexion.php
@@ -27,11 +27,11 @@
         <form action="" method="post">
             <div class="form-group" id="formdiv">
                 <label for="pseudo" class="loginLabel">Pseudo</label>
-                <input type="text" name="uid" class="form-control" id="input" placeholder="pseudo">
+                <input type="text" name="uid" class="form-control loginInputs" id="inputUid" placeholder="pseudo">
             </div>
             <div class="form-group">
                 <label for="password" class="loginLabel">Mot de passe</label>
-                <input type="password" name="pwd" class="form-control" id="input" placeholder="Mot de passe">
+                <input type="password" name="pwd" class="form-control loginInputs" id="inputPwd" placeholder="Mot de passe">
             </div>
             <button type="submit" id="loginSubmit" class="btn btn-primary">Valider</button>
         </form>
@@ -47,7 +47,7 @@
 
 
     </div>
-
+    <script src="<?php echo $root . "/static/js/functions.js" ?>"></script>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
